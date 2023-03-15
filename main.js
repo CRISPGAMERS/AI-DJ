ef898ab47d60fc0c3c60f4dc0a42758710855057
CRISPGAMERS <98216094+CRISPGAMERS@users.noreply.github.com>
2023-03-15 12:52:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 sound="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0
+rightWristY=0
 function preload()
 {
     sound = loadSound("music.mp3");
+
+    sound.setVolume(1);
+    sound.rate(1);
 }
 
 function setup()
@@ -11,6 +18,29 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on("pose", gotposes);
+}
+
+function gotposes(results)
+{
+if(results.length>0)
+{
+    console.log(results);
+
+    leftWristX= results[0].pose.leftWrist.x;
+    leftWristY= results[0].pose.leftWrist.y;
+    console.log("leftWristX = "+ leftWristX + "leftWristY = " + leftWristY);
+
+    rightWristX= results[0].pose.rightWrist.x;
+    rightWristY= results[0].pose.rightWrist.y;
+    console.log("rightWristX = "+ rightWristX + "rightWristY = " + rightWristY);
+}
+}
+
+function modelLoaded()
+{
+    console.log("model is initialized");
 }
 
 function draw()
